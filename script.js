473d4bf1c2fa8f7c80f9fc7438cb38d2af069e8e
Dewaley <adewaleademilua@gmail.com>
2021-01-07 17:55:06 +0100

    Slider

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ let sideBar = document.querySelector('.aside-right')
 let hamburger = document.querySelector('.hamburger')
 
 hamburger.addEventListener('click', (e) => {
- hamburger.classList.toggle('clicked')
- sideBar.classList.toggle('slide')
+  hamburger.classList.toggle('clicked')
+  sideBar.classList.toggle('slide')
+})
+window.addEventListener('click', (e) => {
+  let getHamburger = document.querySelector('.hamburger')
+  let checkClassList = getHamburger.classList.contains('clicked')
+  let checkClicked = e.target.classList.contains('hamburger')
+  if (checkClassList && !checkClicked) {
+    hamburger.classList.toggle('clicked')
+    sideBar.classList.toggle('slide')
+  }
 })
