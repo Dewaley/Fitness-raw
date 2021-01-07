2be4dce8fbca70cd5adec85ae315ea22289ad3c7
mrwaccaff <36827870+mrwaccaff@users.noreply.github.com>
2021-01-07 12:07:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ hamburger.addEventListener('click', (e) => {
  hamburger.classList.toggle('clicked')
  sideBar.classList.toggle('slide')
 })
+
+window.addEventListener('click', (e) => {
+ let getHamburger = document.querySelector('.hamburger');
+ let checkClassList = getHamburger.classList.contains('clicked')
+ let checkClicked = e.target.classList.contains('hamburger')
+ if(checkClassList && !checkClicked){
+ hamburger.classList.toggle('clicked')
+ sideBar.classList.toggle('slide')
+    }
+
+})
